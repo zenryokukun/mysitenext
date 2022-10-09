0877f0d731365875ab2553b8adaa20b0527b38b0
crypto <bathingasian001@gmail.com>
2022-10-09 12:52:43 +0900

    fixed bugs

--- lib/build.sh
+++ lib/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo Now,building....
 export NODE_ENV=production
+# You need to be in `root` in order to use `next build` command
+cd /home/crypto/mysitenext
+# Build next app.
 npx next build
-# `nextblog` is the app name set to PM2.
+# Restart server. `nextblog` is the app name set to PM2.
 pm2 restart nextblog
