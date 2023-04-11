d5ffe02550ea7d4fecb5fe80e62cd32b4d1d6020
crypto <bathingasian001@gmail.com>
2023-04-11 22:54:23 +0900

    delete meta charset, due to duplication

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <meta charSet="UTF-8" />
+        {/* 勝手に付けてくれるようなのでコメントアウト。 */}
+        {/* <meta charSet="UTF-8" /> */}
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"></link>
       </Head>
