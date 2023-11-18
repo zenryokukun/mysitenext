1c553f48b346e97a57399beb52d9b69d6f11b0b4
crypto <bathingasian001@gmail.com>
2023-11-18 21:56:55 +0900

    add node-windows page

---
+++ app/new-post/node-windows/page.tsx
@@ -0,0 +1,33 @@
+/**
+ * 
+ * .mdx版ブログページのテンプレート。コピペして`DIR`のみ編集でいけるようにする。
+ * 今後の修正を減らすため、ここでstylingや不要な処理は加えないように！
+ * 
+ * [slug]で動的にmdxをimportしようとするとエラーになり、解決できなかった。
+ * やむなく、MdxLayoutにラップしてmdxを渡す形式に変更。
+ * 
+ */
+
+// mdxページ用汎用レイアウト。従来の.md版ページと同じにしている。
+import MdxLayout from "../MdxLayout";
+import generateMeta from "../generateMeta";
+// mdxファイルでexportされるオブジェクト。meta情報と、frontmatter部分の情報。
+import { mdxMeta, frontMatter } from "./page.mdx";
+// mdxのコンテンツ
+import Content from "./page.mdx";
+
+import type { Metadata } from "next";
+
+const DIR = "node-windows";
+
+export const metadata: Metadata = generateMeta(mdxMeta, DIR)
+
+
+export default function Page() {
+  return (
+    /* @ts-expect-error Server Component */
+    <MdxLayout dir={DIR} frontMatter={frontMatter} >
+      <Content></Content>
+    </MdxLayout >
+  );
+}
