83dbf79b5ca04216add9559d99803bf7cf6abeb0
crypto <bathingasian001@gmail.com>
2024-01-15 21:09:56 +0900

    change <Script /> to <script />, since ads are not showing

--- app/component/Adsense.tsx
+++ app/component/Adsense.tsx
@@ -2,11 +2,10 @@
  * Google Adsense用Scriptコンポーネント
  */
 
-import Script from "next/script";
-
 export default function Adsense() {
   return (
-    <Script
+    <script
+      async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5798674593050703"
       crossOrigin="anonymous"
     />
