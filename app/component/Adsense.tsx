1008bf4f3e46550bbbdb5f66866f9d92597718ed
crypto <bathingasian001@gmail.com>
2024-01-15 22:03:41 +0900

    change script to Script with fterInteractive strategy

--- app/component/Adsense.tsx
+++ app/component/Adsense.tsx
@@ -2,12 +2,14 @@
  * Google Adsense用Scriptコンポーネント
  */
 
+import Script from "next/script";
+
 export default function Adsense() {
   return (
-    <script
-      async
+    <Script
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5798674593050703"
       crossOrigin="anonymous"
+      strategy="afterInteractive"
     />
   );
 }
