b16c52d4e5f34c6e9b6fddd20a44fd7fb774ce4e
crypto <bathingasian001@gmail.com>
2024-04-03 23:10:33 +0900

    remove unused page router

--- pages/api/admin/testbuild.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { buildTest } from "../../../lib/build"
-
-export default function testBuild(req: NextApiRequest, res: NextApiResponse) {
-    console.log("testBuild API called")
-    buildTest();
-    res.end();
-}
