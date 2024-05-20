1b78ef958fb59ee2dc685c3e90dc8a69f45617ab
crypto <bathingasian001@gmail.com>
2024-05-20 22:08:31 +0900

    add nextjs-mdx-remote page

---
+++ app/new-post/nextjs-mdx-remote/page.tsx
@@ -0,0 +1,32 @@
+/**
+ * 
+ * .mdx版ブログページのテンプレート。コピペして`DIR`のみ編集でいけるようにする。
+ * 今後の修正を減らすため、ここでstylingや不要な処理は加えないように！
+ * 
+ * [slug]で動的にmdxをimportしようとするとエラーになり、解決できなかった。
+ * やむなく、MdxLayoutにラップしてmdxを渡す形式に変更。
+ * 
+ */
+
+// mdxページ用汎用レイアウト。従来の.md版ページと同じにしている。
+import MdxLayout from "../MdxLayout";
+import generateMeta from "../generateMeta";
+// mdxファイルでexportされるオブジェクト。meta情報と、frontmatter部分の情報。
+import { mdxMeta, frontMatter } from "./page.mdx";
+// mdxのコンテンツ
+import Content from "./page.mdx";
+
+import type { Metadata } from "next";
+
+const DIR = "nextjs-mdx-remote";
+
+export const metadata: Metadata = generateMeta(mdxMeta, DIR)
+
+
+export default function Page() {
+  return (
+    <MdxLayout dir={DIR} frontMatter={frontMatter} >
+      <Content></Content>
+    </MdxLayout >
+  );
+}
