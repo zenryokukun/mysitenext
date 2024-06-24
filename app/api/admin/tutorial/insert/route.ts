aab13e1f8d83728a6b4b256fbfd001ca3fac76c1
crypto <bathingasian001@gmail.com>
2024-06-24 21:50:51 +0900

    add revalidatePath

--- app/api/admin/tutorial/insert/route.ts
+++ app/api/admin/tutorial/insert/route.ts
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 import extract from "../extract-form";
 import { isAdmin } from "../../../../../lib/db/admin";
@@ -18,9 +19,13 @@ export async function POST(req: NextRequest) {
     const cols = extract(fd);
     const result = await insertTutorial(cols);
     const msg = result.acknowledged ? "挿入は成功です☆" : "挿入に失敗しました...";
-    return new NextResponse(msg, {
+    const resp = new NextResponse(msg, {
         status: 200,
         // charsetが無いとブラウザで文字化けするため、明示。
         headers: { "Content-Type": "text/plain; charset=UTF-8" }
-    })
+    });
+    if (result.acknowledged) {
+        revalidatePath("/tutorial");
+    }
+    return resp;
 }
